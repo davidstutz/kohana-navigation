dd4ede91c1d519e78632958fc00722e99830a7c2
David Stutz <davidstutz@web.de>
2013-06-24 21:16:19 +0200

    Deleted line driver, added header driver instead. Added theme support, currently only bootstrap theme. Minor fixes.

--- views/navigation/bootstrap/driver/header.php
+++ views/navigation/bootstrap/driver/header.php
@@ -1,6 +1,10 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
+ * @param <object> item
+ * 
  * @author David Stutz
  */
 ?>
-<li class="divider"></li>
+<li class="nav-header">
+	<?php echo __($item->label); ?>
+</li>
